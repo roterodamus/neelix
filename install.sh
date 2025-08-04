1526495e5e9ae66ccef01969bdc7fa535ef39820
Roterodamus <89250803+roterodamus@users.noreply.github.com>
2025-08-04 02:19:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -191,6 +191,7 @@ ln -sr ~/.config/Wallpapers ~/Pictures/Wallpapers
 
 sudo cp misc/remapperoverview/remapoverview.service /etc/systemd/system/remapoverview.service
 sudo cp misc/remapperoverview/remapoverview.sh /usr/local/bin/remapoverview.sh
+sudo chmod +x /usr/local/bin/remapoverview.sh
 sudo mkdir /etc/keyd/
 sudo cp misc/remapperoverview/keyd/overview-open.conf /etc/keyd/overview-open.conf
 sudo cp misc/remapperoverview/keyd/overview-closed.conf /etc/keyd/overview-closed.conf
