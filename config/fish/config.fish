a02409ca26b0d752f55cc3399ab655f10e49b6e2
Roterodamus <89250803+roterodamus@users.noreply.github.com>
2026-01-02 01:54:43 +0100

    Add files via upload

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,3 +4,28 @@ end
 
 function fish_greeting
 end
+
+set -x SUDO_EDITOR /usr/bin/helix
+set -x NEWT_COLORS "root=white,black \
+        border=black,lightgray \
+        window=lightgray,lightgray \
+        shadow=black,gray \
+        title=black,lightgray \
+        button=black,gray \
+        actbutton=black,lightgray \
+        compactbutton=black,lightgray \
+        checkbox=black,lightgray \
+        actcheckbox=lightgray,black \
+        entry=black,lightgray \
+        disentry=gray,lightgray \
+        label=black,lightgray \
+        listbox=black,lightgray \
+        actlistbox=black,white \
+        sellistbox=lightgray,black \
+        actsellistbox=lightgray,black \
+        textbox=black,lightgray \
+        acttextbox=black,white \
+        emptyscale=,gray \
+        fullscale=,white \
+        helpline=white,black \
+        roottext=lightgrey,black"
