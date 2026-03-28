a5c8457a08f9ab42e26fd3290c579ec31b7808c2
Roterodamus <89250803+roterodamus@users.noreply.github.com>
2026-03-28 23:35:10 +0100

    Remove Docker and UFW setup from install.sh
    Removed Docker configuration and firewall setup steps from install script.

--- install.sh
+++ install.sh
@@ -81,41 +81,6 @@ sudo systemctl set-default graphical.target
 
 yay -Syu --needed --noconfirm - < <(grep -v '^#' packages.txt | grep -v '^$')
 
-# Limit log size to avoid running out of disk
-sudo mkdir -p /etc/docker
-echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json
-
-# Give this user privileged Docker access
-sudo usermod -aG docker $USER
-
-# Prevent Docker from preventing boot for network-online.target
-sudo mkdir -p /etc/systemd/system/docker.service.d
-sudo tee /etc/systemd/system/docker.service.d/no-block-boot.conf <<'EOF'
-[Unit]
-DefaultDependencies=no
-EOF
-
-# Enable ufw and docker services
-
-sudo systemctl daemon-reload
-
-sudo systemctl enable docker.service
-sudo systemctl enable ufw.service
-
-# Allow nothing in, everything out
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
-# Allow ports for LocalSend
-sudo ufw allow 53317/udp
-sudo ufw allow 53317/tcp
-# Allow SSH in
-sudo ufw allow 22/tcp
-# Allow Docker containers to use DNS on host
-sudo ufw allow in on docker0 to any port 53
-# Turn on the firewall
-sudo ufw enable
-sudo ufw reload
-
 # =======================================================
 # Enable flatpak and bazaar
 # =======================================================
