b5bd56edb3379de5ddcef5e897a06416dd0c297a
Roterodamus <89250803+roterodamus@users.noreply.github.com>
2026-03-29 17:16:00 +0200

    Enable foot-server.service and install stable Rust

--- post_install/install_devtools.sh
+++ post_install/install_devtools.sh
@@ -2,5 +2,6 @@
 
 sudo pacman -Sy --needed --noconfirm ripgrep jq taplo-cli marksman rustup lldb rust-analyzer
 
-rustup install stable
+systemctl --user enable foot-server.service
 
+rustup install stable
