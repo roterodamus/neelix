1ed24b42874ccde9564525d4361851d9289956c8
Roterodamus <89250803+roterodamus@users.noreply.github.com>
2026-03-29 16:17:10 +0200

    Install Flatpak using pacman
    Add installation command for Flatpak using pacman

--- post_install/install_flatpak.sh
+++ post_install/install_flatpak.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo pacman -Sy --needed --noconfirm flatpak
+
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 flatpak install -y --noninteractive flathub io.github.kolunmi.Bazaar
